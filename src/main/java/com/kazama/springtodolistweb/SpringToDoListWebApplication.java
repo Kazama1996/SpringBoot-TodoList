b30d6eb47bf3f148791f97db3f309780da391c1a
Kazama1996 <Y.Kazama1996@gmail.com>
2022-07-29 16:20:34 +0800

    initial commit

---
+++ src/main/java/com/kazama/springtodolistweb/SpringToDoListWebApplication.java
@@ -0,0 +1,13 @@
+package com.kazama.springtodolistweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringToDoListWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringToDoListWebApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/kazama/springtodolistweb/model/task.java
@@ -0,0 +1,11 @@
+package com.kazama.springtodolistweb.model;
+
+import java.util.Date;
+
+public class task {
+        String taskName;
+        Date createDate;
+        Date deadLine;
+
+}
+
